483591a3290c8a4ff78abee7fb00521716499676
jiedong-yang <jyang92@hawk.iit.edu>
2023-04-15 05:03:56 -0500

    update: path

--- src/facerender/animate.py
+++ src/facerender/animate.py
@@ -145,7 +145,7 @@ class AnimateFromCoeff():
 
         predictions_video = make_animation(source_image, source_semantics, target_semantics,
                                         self.generator, self.kp_extractor, self.he_estimator, self.mapping, 
-                                        yaw_c_seq, pitch_c_seq, roll_c_seq, use_exp = True)
+                                        yaw_c_seq, pitch_c_seq, roll_c_seq, use_exp=True)
 
         predictions_video = predictions_video.reshape((-1,)+predictions_video.shape[2:])
         predictions_video = predictions_video[:frame_num]
@@ -178,16 +178,16 @@ class AnimateFromCoeff():
         sound = AudioSegment.from_mp3(audio_path)
         frames = frame_num 
         end_time = start_time + frames*1/25*1000
-        word1=sound.set_frame_rate(16000)
+        word1 = sound.set_frame_rate(16000)
         word = word1[start_time:end_time]
         word.export(new_audio_path, format="wav")
 
-        save_video_with_watermark(path, new_audio_path, av_path, watermark= False)
+        save_video_with_watermark(path, new_audio_path, av_path, watermark=False)
         print(f'The generated video is named {video_name} in {video_save_dir}')
 
         if preprocess.lower() == 'full':
             # only add watermark to the full image.
-            video_name_full = x['video_name']  + '_full.mp4'
+            video_name_full = x['video_name'] + '_full.mp4'
             full_video_path = os.path.join(video_save_dir, video_name_full)
             return_path = full_video_path
             paste_pic(path, pic_path, crop_info, new_audio_path, full_video_path)
